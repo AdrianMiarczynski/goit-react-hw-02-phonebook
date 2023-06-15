20ca1464575c4ce709658d20ef4ff411f3f5fb42
Adrian Miarczyński <adrian331144@gmail.com>
2023-06-15 17:55:10 +0200

    first approach to the form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactsForm from './contacts/contacts';
+// import { nanoid } from 'nanoid';
+
+export class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactsForm onSubmit={values => console.log(values)} />
+        {/* <div>
+          <h2>Contacts</h2>
+        </div> */}
+      </div>
+    );
+  }
+}
+export default App;
