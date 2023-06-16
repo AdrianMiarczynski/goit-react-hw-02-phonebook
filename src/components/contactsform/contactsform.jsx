1bde321c1cec4a35e781909690d21f4a32c421c5
Adrian Miarczyński <adrian331144@gmail.com>
2023-06-16 21:52:08 +0200

    remove contacts and numberst to contactsform

--- src/components/contactsform/contactsform.jsx
+++ src/components/contactsform/contactsform.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import css from './contactsform.module.css';
 
 class ContactsForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
   render() {
-    const { name } = this.props.name;
-    const { number } = this.props.number;
+    const { name } = this.state.name;
+    const { number } = this.state.number;
     return (
       <form onSubmit={this.props.handlerSubmit} className={css.form}>
         <label htmlFor="name" className={css['form__label']}>
@@ -17,7 +21,7 @@ class ContactsForm extends Component {
             className={css['form__input']}
             name="name"
             id="name"
-            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -27,7 +31,7 @@ class ContactsForm extends Component {
           <input
             type="tel"
             name="number"
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             id="number"
@@ -46,8 +50,6 @@ class ContactsForm extends Component {
 export default ContactsForm;
 
 ContactsForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
   handlerSubmit: PropTypes.func.isRequired,
   handlerChange: PropTypes.func.isRequired,
 };
